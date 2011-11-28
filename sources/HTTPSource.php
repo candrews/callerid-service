f1d647c3c4680974338857f8b37046ebfa25e02d
Craig Andrews <candrews@integralblue.com>
2011-11-27 23:41:44 -0500

    Make the $post_data argument actually work (and not use a function that doesn't exist)

--- sources/HTTPSource.php
+++ sources/HTTPSource.php
@@ -61,7 +61,7 @@ abstract class HTTPSource extends Source
 	    }
 	    if($post_data){
 		    curl_setopt($crl, CURLOPT_POST, 1); // set POST method
-		    curl_setopt($crl, CURLOPT_POSTFIELDS, cisf_url_encode_array($post_data)); // add POST fields
+		    curl_setopt($crl, CURLOPT_POSTFIELDS, $post_data); // add POST fields
 	    }
 	    return $crl;
     }
