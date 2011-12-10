7af3106e97f5263babda92bb6bbb69ca39813c01
Craig Andrews <candrews@integralblue.com>
2011-12-10 15:55:12 -0500

    Add tests for no results lookups

--- testSources.php
+++ testSources.php
@@ -22,40 +22,58 @@ class SourcesTest extends PHPUnit_Framework_TestCase
     public function provider(){
         return array(
             array('AddressesSource', '+17817498755', 'us', 'Lynn D Donovan', '36 Myers Farm Rd, Hingham, MA 02043'),
+            array('AddressesSource', '+18002927508', 'us', null),
             array('AddressesYellowPagesSource', '+18604297433', 'us', 'Willington Pizza House', '25 River Rd, Willington, CT 06279'),
-            array('CanpagesSource', '+15147371111', 'ca', 'Pizza Pizza', '5184 av du Parc, Montréal, QC'),
+            array('AddressesYellowPagesSource', '+18002927508', 'us', null),
+            array('CanpagesSource', '+18002927508', 'ca', null),
             array('CitizensInfoSource', '+12169215024', 'us', 'Able Dixon', '3259 Van Aken Blvd, Shaker Heights, OH 44120'),
+            array('CitizensInfoSource', '+18002927508', 'us', null),
             array('CountrySource', '+17817498755', 'us', 'Unknown (United States)', null),
             array('FonectaSource', '+35897012298', 'fi', 'Pizza-Kebab Apollo', 'Vaasankatu 8, 00500 HELSINKI'),
+            array('FonectaSource', '+35896012279', 'fi', null),
             array('HittaSource', '+468291229', 'se', 'John Doe MC', 'Vidjavägen 2 123 52 Farsta'),
+            array('HittaSource', '+468252227', 'se', null),
             array('InfobelSource', '+390744433549', 'it', 'ALBERGO RISTORANTE LOCANDA DEL VECCHIO MAGLIO (CENTRALINO ALBERGO RISTORANTE)', 'VLE BRIN 148, 05100 TERNI'),
             array('InfobelSource', '+493041726953', 'de', 'Jones Brigitte', 'Trautenaustr. 11, 10717 Berlin'),
+            array('InfobelSource', '+493041726954', 'de', null),
             array('LocalTelSource', '+41264244141', 'ch', 'A-Allô Pizza', 'route de la Glâne 7, 1700 Fribourg'),
+            array('LocalTelSource', '+41254254141', 'ch', null),
             array('PagineBiancheSource', '+390697605917', 'it', 'AL CASALE PIZZERIA - Di ALESSIO E NOEMI TRIBINI Snc', 'Via Rocca Fiorita 155 - 00133 Valle Fiorita (RM)'),
             array('PagineBiancheSource', '+390521830398', 'it', 'Jones Alan', 'Via Calestano 202 - 43035 Felino (PR)'),
+            array('PagineBiancheSource', '+390522730498', 'it', null),
             array('PersonLookupSource', '+61393952708', 'au', 'Jones A', '12 Caledonian Way, Point Cook, VIC 3030'),
+            array('PersonLookupSource', '+61393962708', 'au', null),
             array('TelcoDataSource', '+13137370000', 'us', 'DETROITZN5, MI', 'DETROITZN5, MI'),
-            array('UkPhoneInfoSource', '+442075891278', 'uk', 'London', 'London'),
+            array('TelcoDataSource', '+18002927508', 'us', null),
+            array('UkPhoneInfoSource', '+442074862080', 'uk', 'London', 'London'),
+            array('UkPhoneInfoSource', '+442175891278', 'uk', null),
             array('WhitePagesSource', '+17817498755', 'ca', 'Lynn D Donovan', 'Myers Farm Rd, Hingham, MA'),
+            array('WhitePagesSource', '+18002927508', 'ca', null),
             array('YellowPagesBusinessSource', '+18604297433', 'us', 'Willington Pizza House', '25 River Rd, Willington, CT 06279'),
             array('YellowPagesPersonSource', '+17817498755', 'us', 'Lynn D Donovan', '36 Myers Farm Rd, Hingham, MA 02043'),
             array('YellowPagesPersonSource', '+12016608433', 'us', 'Jae L Choi', '119 Bogerts Mill Rd, Harrington Park, NJ 07640'),
-            array('YellowPages_PTSource', '+351219560449', 'pt', 'Xou Pizza-Pizzaria e Churrasqueira Sociedade Lda', 'Avenida Ernest Solvay 5-lj 5, 2625-168 PÓVOA DE SANTA IRIA')
+            array('YellowPagesPersonSource', '+18002927508', 'us', null),
+            array('YellowPages_PTSource', '+351219560449', 'pt', 'Xou Pizza-Pizzaria e Churrasqueira Sociedade Lda', 'Avenida Ernest Solvay 5-lj 5, 2625-168 PÓVOA DE SANTA IRIA'),
+            array('YellowPages_PTSource', '+351319570449', 'pt', null)
         );
     }
     
     /**
      * @dataProvider provider
      */
-    public function testLookup($sourceName, $thenumber, $country, $expectedName, $expectedAddress){
+    public function testLookup($sourceName, $thenumber, $country, $expectedName, $expectedAddress=null){
         $source = new $sourceName();
         $source->thenumber = $thenumber;
         $source->country = $country;
         $this->assertTrue($source->prepare());
         $result = $source->lookup();
-        $this->assertTrue($result!==false);
-        $this->assertEquals($expectedName, $result->name);
-        $this->assertEquals($expectedAddress, $result->address);
+        if($expectedName == null){
+            $this->assertFalse($result);
+        }else{
+            $this->assertTrue($result!==false);
+            $this->assertEquals($expectedName, $result->name);
+            $this->assertEquals($expectedAddress, $result->address);
+        }
     }
 }
 
